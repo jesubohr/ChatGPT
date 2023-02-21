26e1c935b641e9c4ebd832a9a3d506bd2a5bddea
Jesús Borrero <jesufer2018@hotmail.com>
2023-02-20 23:52:56 -0500

    Add scroll functionality to container scroll button

--- src/components/Chat/ChatView.tsx
+++ src/components/Chat/ChatView.tsx
@@ -1,25 +1,59 @@
 import type { Message } from '@/types'
-import { ArrowDownIcon } from '../Icons'
+import { useRef, useState } from 'react'
 import { MessageView } from './Message'
+import { ArrowDownIcon } from '../Icons'
 
 interface ChatViewProps {
   messages: Message[]
 }
 export function ChatView({ messages }: ChatViewProps) {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [isAtBottom, setIsAtBottom] = useState(false)
+
+  function handleScroll() {
+    if (containerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = containerRef.current
+      setIsAtBottom(scrollTop + clientHeight >= scrollHeight)
+    }
+  }
+
+  function scrollToBottom() {
+    if (containerRef.current) {
+      // Scroll to bottom smoothly
+      containerRef.current.scrollTo({
+        top: containerRef.current.scrollHeight,
+        behavior: 'smooth'
+      })
+    }
+  }
+
   return (
-    <div className='w:full h:full overflow-y:auto'>
+    <div
+      ref={containerRef}
+      onScroll={handleScroll}
+      className='chat-container w:full h:full overflow-y:auto'
+    >
       <section className='flex flex:col ai:center'>
         {messages.map(message => (
-          <MessageView key={message.id} isAI={message.ai} message={message.text} />
+          <MessageView
+            key={message.id}
+            isAI={message.ai}
+            message={message.text}
+          />
         ))}
         <div className='w:full h:8rem h:12rem@sm flex-shrink:0' />
       </section>
-      <button
-        type='button'
-        className='abs right:24 bottom:120 p:4 color:gray-80 bg:white/.1 b:1|solid|white/.1 r:100% z:10'
-      >
-        <ArrowDownIcon />
-      </button>
+      {
+        !isAtBottom && (
+          <button
+            type='button'
+            onClick={scrollToBottom}
+            className='abs right:24 bottom:120 p:4 color:gray-80 bg:white/.1 b:1|solid|white/.1 r:100% z:10'
+          >
+            <ArrowDownIcon />
+          </button>
+        )
+      }
     </div>
   )
 }
